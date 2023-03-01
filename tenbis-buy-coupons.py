d123673560e19479fafa554c3fc222ed1c932b12
Dvir Perets <dvirus89@gmail.com>
2023-03-01 08:31:44 +0200

    cleanup

--- tenbis-buy-coupons.py
+++ tenbis-buy-coupons.py
@@ -37,16 +37,6 @@ def get_coupons_mixture(budget):
     return min_coupon, cl
 
 def main_procedure():
-    # If token exists, use the token to authenticate 10bis
-    #if os.path.exists(SESSION_PATH) and os.path.exists(TOKEN_PATH):
-        #session = load_pickle(SESSION_PATH)
-        #user_token = load_pickle(TOKEN_PATH)
-        #session.user_token = user_token
-
-    # If there's no token, authenticate 10bis and extract auth tokens
-    #else:
-        #session = auth_tenbis()
-        #create_pickle(session,SESSION_PATH)
     session = auth_tenbis()
     # get budget
     budget = get_available_budget(session)
@@ -59,36 +49,11 @@ def main_procedure():
         print(f"Result: {num_of_coupons} coupons to buy: {coupons_mixture}")
         for coupon in range(0, len(coupons_mixture),1):
             print(f"Buying coupon #{coupon+1}: {coupons_mixture[coupon]}")
-            #get_shopping_card_guid(session)
             buy_coupon(session,coupons_mixture[coupon])
             print("waiting two minutes before the next one...\r\n")
             time.sleep(130)
     else:
         print(f"Sorry, your budget ({budget}) is lower than the smallest available coupon ({min(COUPONS_TYPES)}).")
-
-def input_number(message):
-  while True:
-    try:
-       userInput = int(input(message))       
-    except ValueError:
-       print("Not an integer! Try again. (examples: 1,2,3,4,5)")
-       continue
-    else:
-       return userInput 
-       break 
-
-def write_file(path, content):
-    with open(path, 'w', encoding='utf-8') as f:
-        f.write(content)
-
-def create_pickle(obj, path):
-    with open(path, 'wb') as session_file:
-        pickle.dump(obj, session_file)
-
-def load_pickle(path):
-    with open(path, 'rb') as session_file:
-        objfrompickle = pickle.load(session_file)
-        return objfrompickle
 
 def get_available_budget(session):
     endpoint = TENBIS_FQDN + "/NextApi/UserTransactionsReport"
@@ -103,10 +68,6 @@ def get_available_budget(session):
     return budget
 
 def buy_coupon(session, coupon):
-    # add to shopping cart
-    # SetAddressInOrder
-    #
-    #session.cart_guid = get_shopping_card_guid(session) #test generate uuid -  remove?
     endpoint = TENBIS_FQDN + f"/NextApi/SetAddressInOrder"
     headers = {"content-type": "application/json"}
     headers.update({'user-token': session.user_token})
@@ -166,7 +127,7 @@ def buy_coupon(session, coupon):
         print(resp_json)
         input("wait log SetDishListInShoppingCart")
 
-# GetPayments
+    # GetPayments
     #
     endpoint = TENBIS_FQDN + f"/NextApi/GetPayments?shoppingCartGuid={session.cart_guid}&culture=he-IL&uiCulture=he"
     headers = {"content-type": "application/json"}
@@ -210,25 +171,10 @@ def buy_coupon(session, coupon):
         input("wait log SubmitOrder")
     session.cart_guid = resp_json['ShoppingCartGuid']
 
-
-def get_shopping_card_guid(session):
-    endpoint = TENBIS_FQDN + f"/NextApi/GetPayments?shoppingCartGuid=00000000-0000-0000-0000-000000000000&culture=he-IL&uiCulture=he"
-    headers = {"content-type": "application/json"}
-    headers.update({'user-token': session.user_token})
-    response = session.get(endpoint, headers=headers, verify=False)
-    resp_json = json.loads(response.text)
-    if(DEBUG):
-        print("Request:\r\n" + endpoint + "\r\n########")
-        print("Response: " + str(response.status_code) + "\r\n")
-        print(resp_json['ShoppingCartGuid'])
-        input("wait log GetPayments to extract shopping cart guid")
-    return resp_json['ShoppingCartGuid']
-
 def auth_tenbis():
     # Phase one -> Email
     email = input("Enter email: ")
     endpoint = TENBIS_FQDN + "/NextApi/GetUserAuthenticationDataAndSendAuthenticationCodeToUser"
-
     payload = {"culture": "he-IL", "uiCulture": "he", "email": email}
     headers = {"content-type": "application/json"}
     session = requests.session()
@@ -258,7 +204,6 @@ def auth_tenbis():
     response = session.post(endpoint, data=json.dumps(payload), headers=headers, verify=False)
     resp_json = json.loads(response.text)
     user_token = resp_json['Data']['userToken']
-    create_pickle(user_token, TOKEN_PATH)
     session.user_token = user_token
     session.cart_guid = resp_json['ShoppingCartGuid']
     session.user_id = resp_json['Data']['userId']
